54bbd081e5b7fcd5df654f13c740cf5e5ace78d6
Admin <Admin@DESKTOP-UPB6D1O>
2018-04-09 15:34:35 +0700

    bbb

---
+++ FASTFOOD/Controllers/StaffController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FASTFOOD.Controllers
+{
+    public class StaffController : Controller
+    {
+        // GET: Staff
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
